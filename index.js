893085b53defc2e8b49de09b55e0033a64095e1c
Petar Cale <usavatreni@gmail.com>
2020-11-17 02:19:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
   newObj[key] = value;
   return newObj;
 }
+
+destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object;
+}
+
